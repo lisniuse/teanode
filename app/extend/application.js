05b0110be671c75bc33071d84b0bcb9879366525
lisniuse <17560235@qq.com>
2018-07-20 15:59:37 +0800

    init dev.

--- app/extend/application.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// 扩展一些框架便利的方法
-module.exports = {};
--- app/middleware/less.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-const loader = require('loader-koa');
-
-module.exports = (options, app) => {
-  return loader.less(path.join(app.baseDir, 'app'));
-};
